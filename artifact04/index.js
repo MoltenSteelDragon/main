067198b6cfbe485b24eb71e209c0da42db42dae9
MoltenSteelDragon <130028003+MoltenSteelDragon@users.noreply.github.com>
2023-05-27 23:06:32 -0700

    Update index.js

--- artifact04/index.js
+++ artifact04/index.js
@@ -1,108 +1,47 @@
-function checkForm(form){
-  var errors=[];
-  
-  //FIRST NAME
-  //check for blank input
-  if(form.FirstName.value === "")
-    errors.push("First Name is empty")
-  
-  var re =  /^[A-Za-z]+$/;
-  
-  //checks for invalid characters
-  if(!re.test(form.FirstName.value) && form.FirstName.value !== "" )
-     errors.push("First Name contains invalid character");
-  
-  //checks to see if intput is too long
-  if(form.FirstName.value.length > 20)
-     errors.push("First Name is too long");
-  
-  //LAST NAME
-  //check for blank input
-  if(form.LastName.value === "")
-    errors.push("Last Name is empty");
-  
-  //checks for invalid characters
-  if(!re.test(form.LastName.value) && form.LastName.value !== "" )
-     errors.push("Last Name contains invalid character");
-  
-  //checks to see if intput is too long
-  if(form.LastName.value.length > 50)
-     errors.push("Last Name is too long");
-  
-  //EMAIL
-  //check for blank input
-  if(form.EMail.value === "")
-    errors.push("EMail is empty")
-  
-  //PHONE
-  //check for blank input
-  if(form.Phone.value === "")
-    errors.push("Phone is empty")
-    
-    re = /^[0-9]+$/;
-  
-  //checks for invalid characters
-  if(!re.test(form.Phone.value) && form.Phone.value !== "" )
-     errors.push("Phone contains invalid character");
-  
-  //checks to see if intput is too long
-  if(form.Phone.value.length > 15)
-     errors.push("Phone is too long");
-  
-  //USERNAME
-  //check for blank input
-  if(form.Username.value === "")
-    errors.push("Username is empty")
-  
-  //checks to see if intput is too long
-  if(form.Username.value.length >12)
-     errors.push("Username is too long");
-  
-  //PASSWORD
-  //check for blank input
-  if(form.Password.value === "")
-    errors.push("Password is empty")
-    
-  //checks to see if intput is too long
-  if(form.Password.value.length > 7)
-     errors.push("Password is too long");
-  
-  //ADDRESS
-  //check for blank input
-  if(form.Address.value === "")
-    errors.push("Address is empty")
-    
-  //CITY
-  //check for blank input
-  if(form.City.value === "")
-    errors.push("City is empty")
-  
-  //ZIP CODE
-  //check if in USA and zip code is empty
-  if(form.Country.value === "000" && form.ZipCode.value === "")
-    errors.push("ZipCode is required for USA")
-  
-  if(!re.test(form.ZipCode.value) && form.Phone.value !== "" )
-    errors.push("ZipCode contains invalid character");
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  if(errors.length !== 0){
-    var msg = "ERRORS:\n\n"
-    for (var i = 0; i < errors.length; i++)
-      msg += errors[i] + "\n";
-    alert(msg);
-    return false;
-}
-  return true;
-}
+//You should create the validator only after the definition of the HTML form
+var frmvalidator = new Validator("myform");
+frmvalidator.EnableOnPageErrorDisplaySingleBox();
+frmvalidator.EnableMsgsTogether();
+
+frmvalidator.addValidation("FirstName", "req", "Please enter your First Name");
+frmvalidator.addValidation("FirstName", "maxlen=20", "Max length for FirstName is 20");
+frmvalidator.addValidation("FirstName", "alpha_s", "Name can contain alphabetic characters only");
+
+frmvalidator.addValidation("LastName", "req", "Please enter your Last Name");
+frmvalidator.addValidation("LastName", "maxlen=50", "For LastName, Max length is 50");
+frmvalidator.addValidation("LastName", "alpha_s", "Name can contain alphabetic characters only");
+
+frmvalidator.addValidation("Email", "req");
+
+frmvalidator.addValidation("Phone", "req", "Please enter your Phone number");
+frmvalidator.addValidation("Phone", "maxlen=15", "For Phone, Max length is 15");
+frmvalidator.addValidation("Phone", "numeric", "Phone can contain numeric characters only");
+
+frmvalidator.addValidation("Username", "req", "Please enter your Username");
+frmvalidator.addValidation("Username", "maxlen=7", "password max length is 7 characters");
+
+frmvalidator.addValidation("Address", "req", "Please enter your Address");
+
+frmvalidator.addValidation("City", "req", "Please enter your City");
+
+frmvalidator.addValidation("State", "req", "Please enter your State");
+
+
+frmvalidator.addValidation("Zipcode", "req", "Please enter a zipcode", "VWZ_IsListItemSelected(document.forms['myform'].elements['Country'],'US')");
+frmvalidator.addValidation("Zipcode", "maxlen=5", "Please enter a zipcode");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+frmvalidator.addValidation();
