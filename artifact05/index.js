3ecdd2f2c74bf1032264cdb5fac04fcc3c95c712
MoltenSteelDragon <130028003+MoltenSteelDragon@users.noreply.github.com>
2023-06-03 19:42:17 -0700

    Update index.js

--- artifact05/index.js
+++ artifact05/index.js
@@ -1,10 +1,10 @@
-const buttons = document.querySelectorAll("[data-carousel-button]")
+const buttons = document.querySelectorAll("[data-slideshow-button]")
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const offset = button.dataset.slideshowButton === "next" ? 1 : -1
     const slides = button
-      .closest("[data-carousel]")
+      .closest("[data-slideshow]")
       .querySelector("[data-slides]")
 
     const activeSlide = slides.querySelector("[data-active]")
